15f853ec7d3c4be3bb9cc03bbfbf03c287364f5c
gnuion <16549839+gnuion@users.noreply.github.com>
2023-09-04 16:58:21 +0000

    Add webgl examples

---
+++ layouts/webgl.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-[100vh] flex flex-col bg-black">
+    <Header />
+    <!-- <CaoticMovement /> -->
+    <main class="text-lg text-white w-full grow-1 max-w-6xl px-6 pt-20 mx-auto font-mono">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+<style lang="css">
+main a {
+  --at-apply: underline decoration-dotted text-inherit underline-offset-6;
+}
+</style>
