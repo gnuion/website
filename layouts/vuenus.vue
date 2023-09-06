007254bf07149401a6147ac7fa2f76748de9c22c
gnuion <16549839+gnuion@users.noreply.github.com>
2023-09-06 10:06:53 +0000

    Add colorful traingle example

--- layouts/vuenus.vue
+++ layouts/vuenus.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="min-h-[100vh] flex flex-col  bg-black">
-    <Header />
-    <main class="text-lg text-white w-full grow-1 max-w-6xl px-6 pt-20 mx-auto font-mono">
-      <slot />
-    </main>
-    <Footer />
-  </div>
+  <Header />
+  <main class="pt-14 pb-20 bg-black">
+    <slot />
+  </main>
+  <Footer />
 </template>
-<style lang="css">
-main a {
-  --at-apply: underline decoration-dotted text-inherit underline-offset-6;
-
-  &[disabled] {
-    --at-apply: text-gray-7;
-  }
-}
-</style>
+<style lang="css"></style>
