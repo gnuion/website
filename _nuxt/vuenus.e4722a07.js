09e701c4c522281580fc9e6200c293b93841b98d
gnuion <gnuion@users.noreply.github.com>
2023-09-06 10:07:32 +0000

    deploy: da2eb7fb212b1b187b4cdeac661696819ad774e0

---
+++ _nuxt/vuenus.e4722a07.js
@@ -0,0 +1,1 @@
+import{_ as a,a as s}from"./Footer.39d90a1a.js";import{_ as c,c as _,b as e,a as r,A as m,F as p,o as l}from"./entry.1695461b.js";import"./nuxt-link.e30cf831.js";const d={},u={class:"pt-14 pb-20 bg-black"};function f(o,i){const t=a,n=s;return l(),_(p,null,[e(t),r("main",u,[m(o.$slots,"default")]),e(n)],64)}const B=c(d,[["render",f]]);export{B as default};
